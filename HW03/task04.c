48d4d53f31f0f638dc28e2e0a6a7accb8536e442
Sergey Trunov <serzhemail@gmail.com>
2025-10-29 01:59:29 +0300

    hw03

---
+++ HW03/task04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int a, sum;
+	scanf("%3d", &a);
+	sum = a%10;
+	sum += (a/10)%10;
+	sum += (a/100)%10;
+	printf("%d", sum);
+	return 0;
+}
+
